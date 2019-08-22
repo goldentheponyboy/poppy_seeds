2df3ac8f2f03e8d2131f59df5e0f5c2d551f957f
Galina <mur.lechner@gmail.com>
2019-08-22 11:40:32 +0200

    removed unneessary sketch folder

--- sketch_190815a_coingame/end.js
+++
@@ -1,8 +0,0 @@
-
-function endScreen(){
-  textAlign(CENTER);
-  textSize(24);
-  fill(250,0,250);
-  image(moneyCat, width/2, 200, 200, 190)
-  text('score total = ' + score, width/2,50);
-}
--- sketch_190815a_coingame/start.js
+++
@@ -1,7 +0,0 @@
-
-function startScreen(){
-  textAlign(CENTER);
-  textSize(24);
-  fill(250,0,250);
-  text('play game', width/2,height/2);
-}
